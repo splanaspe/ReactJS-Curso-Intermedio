08442ef864494365e39d341ef079573fffcc09d3
splanaspe <splanaspe@uoc.edu>
2022-12-27 11:59:30 +0100

    We've done the main pages and add node repository: axios

---
+++ src/pages/tasks/TaskPages.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskListComponent from '../../components/container/task_list';
+
+
+const TaskPages = () => {
+    return (
+        <div>
+            <TaskListComponent> </TaskListComponent>
+        </div>
+    );
+}
+
+export default TaskPages;
